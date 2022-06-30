887937955632e5c0901298ff0ace191014cae4b2
kasahh <bunmiwhesu@yahoo.com>
2022-06-30 16:18:32 +0000

    size types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,8 @@ int main(void)
 {
 	printf("%lu\n", sizeof(char));
 	printf("%lu\n", sizeof(int));
-	printf("%lu\n", sizeof(float));
-	printf("%lu", sizeof(double));
+	printf("%lu\n", sizeof(long int));
+	printf("%lu", sizeof(long long int));
+	printf("%lu", sizeof(float));
 	return (0);
 }
