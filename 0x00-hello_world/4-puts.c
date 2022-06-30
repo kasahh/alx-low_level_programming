d31807e4eaf5bca48cc633ed55a99a9403895b8b
kasahh <bunmiwhesu@yahoo.com>
2022-06-30 15:31:49 +0000

    putting C in its place

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-int main(void){
-	char str[] = "\"Programming is like building a multilingual puzzle";
-	puts(str);
-	return 0;
+int main(void)
+{
+char str[] = "\"Programming is like building a multilingual puzzle";
+puts(str);
+return (0);
 }
