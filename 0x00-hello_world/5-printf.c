74836ff51fdc351a87ce4ecf877dc2b3b05492fb
kasahh <bunmiwhesu@yahoo.com>
2022-06-30 15:53:42 +0000

    printf when nothing else works

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char sentence[100] = "with proper grammar, but the outcome is a piece of art,";
+
+printf("%s", sentence);
+return (0);
+}
