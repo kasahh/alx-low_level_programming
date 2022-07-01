6f26cc70a7be578b2368897407313238118418fc
kasahh <bunmiwhesu@yahoo.com>
2022-07-01 15:40:21 +0000

    all alphas low and high

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	char l[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+	int ar;
+
+	ar = strlen(l);
+	for (i = 0; i < ar; i++)
+		{ putchar(l[i]); }
+	putchar('\n');
+	return (0);
+}
