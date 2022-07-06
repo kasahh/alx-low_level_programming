fe5b048eacbe46e47b4be6ba6953414568820805
kasahh <bunmiwhesu@yahoo.com>
2022-07-06 21:45:18 +0000

    battery sytreet the og

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,59 @@
+#include "main.h"
+/**
+ * print - print using -putchar
+ *@n: value to print
+ */
+void print(int n);
+/**
+ * print_to_98 - print natural numbers to 98
+ *@val: value to start print from
+ */
+void print_to_98(int val)
+{
+	int max = 98;
 
+	if (val > max)
+	{
+		int i;
+
+		for (i = val; i >= max; i--)
+		{
+			print(i)
+			if (i != max)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+		{
+			int j;
+
+			for (j = val; j <= max; j++)
+			{
+				print(j);
+				if (j != max)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+	}
+	_putchar('\n');
+}
+/**
+ * print - print using -putchar
+ *@n: value to print
+ */
+void print(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		print(n / 10);
+	_putchar(n % 10 + '0');
+}
