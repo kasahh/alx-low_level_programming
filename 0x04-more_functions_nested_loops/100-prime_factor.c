0b20e1305ee9664a780e54082c8c57d2cd072b3a
kasahh <bunmiwhesu@yahoo.com>
2022-07-08 11:56:49 +0000

    largest primes

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints largest prime factor
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	long int var = 612852475143;
+	long int prime = 2;
+
+	while (var > 1)
+	{
+		if (var % prime == 0)
+			var /= prime;
+		else
+			prime++;
+	}
+	print("%ld\n", prime);
+	return (0);
+}
