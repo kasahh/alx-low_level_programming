d3aefad54e1b2c2cde24fdc5bb407ee815a98913
kasahh <bunmiwhesu@yahoo.com>
2022-07-08 01:17:18 +0000

    adding squares to num

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,14 +6,14 @@
 void print_square(int size)
 {
 	if (size < 1)
-		_putchar('\n')
+		_putchar('\n');
 	else
 	{
 		int j;
 
 		for (j = 1; j <= size; j++)
 		{
-			int i
+			int i;
 
 			for (j = 1; j <= size; j++)
 			{
