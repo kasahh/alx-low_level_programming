5e8372c9fa712fae1deefd087f54f8d9cb0b2eca
kasahh <bunmiwhesu@yahoo.com>
2022-07-08 01:14:17 +0000

    adding squares to num

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - print sqauares
+ * @size: size of square
+ */
+void print_square(int size)
+{
+	if (size < 1)
+		_putchar('\n')
+	else
+	{
+		int j;
+
+		for (j = 1; j <= size; j++)
+		{
+			int i
+
+			for (j = 1; j <= size; j++)
+			{
+				int i;
+
+				for (i = 1; i <= size; i++)
+					_putchar('#');
+				_putchar('\n');
+			}
+		}
+	}
+}
