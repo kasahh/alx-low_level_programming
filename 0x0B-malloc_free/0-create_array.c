f4b4565683bddf75fa4f0e25b1d853ce598ded74
kasahh <bunmiwhesu@yahoo.com>
2022-07-26 19:01:32 +0000

    creating arrays

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,7 +20,7 @@ char *create_array(unsigned int size, char c)
 	}
 	if (arr == NULL)
 	{
-		fprintf(sterr, "error allocating memory\n");
+		fprintf(stderr, "error allocating memory\n");
 	}
 	else
 	{
